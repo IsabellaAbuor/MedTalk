a96263f65ce9c869a3ffe87bb3b4c58921baf4b7
Isabella <isabellamabuor@gmail.com>
2023-05-28 15:06:29 +0300

    Login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from "react";
-import { EuiProvider } from "@elastic/eui";
+import { EuiProvider, EuiThemeProvider } from "@elastic/eui";
 import "@elastic/eui/dist/eui_theme_light.css"
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route,  } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 
 function App() {
+
+  const overrides ={
+    colors:{
+      LIGHT:{primary:"#0b5cff"},
+      DARK:{primary:"#0b5cff"},
+    }
+  }
   return (
     <EuiProvider>
-      <BrowserRouter>
-      <Routes>
-        <Route path ="/login" element={<Login />} />
-        <Route path ="/" element={<Dashboard/>} />
-        <Route path ="*" element={<Dashboard/>} />
-      </Routes>
-      </BrowserRouter>
+      <EuiThemeProvider modify = {overrides}>
+        <Routes>
+          <Route path ="/login" element={<Login />} />
+          <Route path ="/" element={<Dashboard/>} />
+          <Route path ="*" element={<Dashboard/>} />
+        </Routes>
+      </EuiThemeProvider>
     </EuiProvider>  
   );
 }
---
+++ src/utilis/FirebaseConfig.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { collection, getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBOgHxtBpd9lvJoK3oNfgvNbqwhpa9Ok0g",
+  authDomain: "client-d0c1e.firebaseapp.com",
+  projectId: "client-d0c1e",
+  storageBucket: "client-d0c1e.appspot.com",
+  messagingSenderId: "771161689187",
+  appId: "1:771161689187:web:1cec5a30f740375b65bc56",
+  measurementId: "G-793K0C41SP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(app);
+export const firebaseDB = getFirestore(app);
