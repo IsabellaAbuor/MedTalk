8f77aa04f921dee3255d8359315ee7e6181fef93
Isabella <isabellamabuor@gmail.com>
2023-05-28 10:14:55 +0300

    Routes created

---
+++ src/app/slices/AuthSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface authIntialState{}
+
+const initialState: authIntialState ={};
+
+export const authSlice =createSlice ({
+    name:"auth",
+    initialState,
+    reducers:{}
+});
+
+export const {} = authSlice.actions;
