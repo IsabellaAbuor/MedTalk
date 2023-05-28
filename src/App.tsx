77ed84a7f17b183feacae655c5158d159ef82e6b
Isabella <isabellamabuor@gmail.com>
2023-05-28 10:14:35 +0300

    Routes path created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { EuiProvider } from "@elastic/eui";
 import "@elastic/eui/dist/eui_theme_light.css"
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
 
 function App() {
   return (
     <EuiProvider>
-      <div>App</div>
+      <BrowserRouter>
+      <Routes>
+        <Route path ="/login" element={<Login />} />
+        <Route path ="/" element={<Dashboard/>} />
+        <Route path ="*" element={<Dashboard/>} />
+      </Routes>
+      </BrowserRouter>
     </EuiProvider>  
-  )
+  );
 }
+
 export default App;
